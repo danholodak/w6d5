7fedb6e433e55aeff01c8cbbb87c84889bf2f7f9
Dan Holodak <danholodak@gmail.com>
2023-02-17 17:49:09 -0500

    add validations

--- cats99/app/models/cat.rb
+++ cats99/app/models/cat.rb
@@ -13,9 +13,20 @@
 #
 
 class Cat < ApplicationRecord 
+	CAT_COLORS = ["brown", "grey", "black", "orange", "amber", "white"].freeze
+
 	validates :birth_date, :name, :color, :sex, presence: true 
 	validates :color, inclusion: {in: CAT_COLORS, message: "Not a valid color"}
 	validates :sex, inclusion: {in: ["M", "F"], message: "We don't respect non-gender pronouns!"}
+	validate :birth_date_cannot_be_future
 
-	CAT_COLORS = ["brown", "grey", "black", "orange", "amber", "white"]
+	
+
+	def birth_date_cannot_be_future
+		today = Date.today#parse(Time.now.to_s.split[0])
+		if (:birth_date <=> today) == 1
+			#sad
+			errors.add(:birthdate, "Cat not born yet!!")
+		end
+	end
 end
